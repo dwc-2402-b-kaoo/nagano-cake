3bebfba0e4150b3d50ffd69629e123d649d80c07
kamasuJL <156296235+kamasuJL@users.noreply.github.com>
2024-03-20 13:12:48 +0000

    break

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,14 +1,14 @@
 class Admin::CustomersController < ApplicationController
-  # before_action :authenticate_admin!
-  
+  before_action :authenticate_admin!
+
   def index
     @customers = Customer.page(params[:page])
   end
-  
+
   def show
     @customer = Customer.find(params[:id])
   end
-  
+
   def edit
     @customer = Customer.find(params[:id])
   end
@@ -21,9 +21,9 @@ class Admin::CustomersController < ApplicationController
       render "edit"
     end
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_active)
   end
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,13 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!, except: [:top]
+  
+  
+  
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_active)
+  end
+  
 end
