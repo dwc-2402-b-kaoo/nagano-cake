66b0c51ba70894968a541e79b7095a421d9fc510
okuraayno <ohsushi555@gmail.com>
2024-03-21 07:56:32 +0000

    admin側機能追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,7 +2,7 @@ class Admin::ItemsController < ApplicationController
 
 # 商品一覧画面用
   def index
-    @items = Item.page(params[:page]).order(created_at: :desc)
+    @items = Item.page(params[:page])
   end
 
 # 商品登録ページ用
@@ -22,6 +22,20 @@ class Admin::ItemsController < ApplicationController
 # 商品詳細画面用
   def show
     @item = Item.find(params[:id])
+  end
+  
+# 商品編集画面用
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
