38c07b5a26bc9b71efb30d2cea1d547fc0777eec
kamasuJL <156296235+kamasuJL@users.noreply.github.com>
2024-03-23 05:20:07 +0000

    fix

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,14 +1,14 @@
 class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def show
     @customer = current_customer
   end
-  
+
   def edit
     @customer = current_customer
   end
-  
+
   def update
     @customer = current_customer
     if @customer.update(customer_params)
@@ -17,13 +17,13 @@ class Public::CustomersController < ApplicationController
       render "edit"
     end
   end
-  
+
   def unsubscribe
   end
-  
+
   def withdraw
   end
-  
+
   private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_active)
